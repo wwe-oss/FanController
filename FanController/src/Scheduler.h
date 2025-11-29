51f7c16a843c022b045d68eba32e2ac23141edf3
Wayne Erdman <wwe-oss>
2025-11-28 20:16:46 -0500

    Initial commit for Input Control.

--- FanController/src/Scheduler.h
+++ FanController/src/Scheduler.h
@@ -3,24 +3,23 @@
 
 #include <Arduino.h>
 
-class TaskScheduler
-{
-public:
-    typedef void (*TaskFn)();
-    struct Task { TaskFn fn; uint32_t intervalMs; uint32_t lastRunMs; bool enabled; };
-
-    static const uint8_t MAX_TASKS = 8;
-
-    TaskScheduler();
-
-    uint8_t addTask(TaskFn fn, uint32_t intervalMs, bool enabled = true);
-    void setTaskInterval(uint8_t idx, uint32_t intervalMs);
-    void enableTask(uint8_t idx, bool enabled);
-    void update(uint32_t nowMs);
-
-private:
-    Task _tasks[MAX_TASKS];
-    uint8_t _count;
+struct Task {
+    uint32_t interval;
+    uint32_t lastRun;
+    void (*callback)();
 };
 
-#endif // SCHEDULER_H
+class Scheduler {
+public:
+    Scheduler();
+
+    void every(uint32_t ms, void (*cb)());
+    void run(uint32_t now);
+
+private:
+    static constexpr uint8_t MAX_TASKS = 8;
+    Task tasks[MAX_TASKS];
+    uint8_t count;
+};
+
+#endif
